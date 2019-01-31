c9d18bed8c943914552d2922220e94f34b28d3c0
Rebecca <angryhamster14@gmail.com>
2019-01-31 10:30:29 -0800

    Finished lab4

---
+++ routes/project.js
@@ -0,0 +1,8 @@
+exports.viewProject = function(req, res){
+	var name = req.params.name; 
+  	console.log("The project name is: " + name);
+  	res.render('project', {
+    	'projectName': name
+  	});
+	
+};
